d6f3be77396d4b0d9d3e00d1662b80f17a1de4b1
Lucas Rosa <lucasmetron@gmail.com>
2026-05-26 11:01:40 -0300

    austes versão

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
-import { readFileSync } from 'fs';
-import usersData from '../data/users.json' with { type: 'json' };
+import { readFileSync } from "fs";
+import usersData from "../data/users.json" with { type: "json" };
 
 // User type
 export type User = {
   username: string;
-  role: 'admin' | 'member';
+  role: "admin" | "member";
   permissions: string[];
   displayName: string;
 };
@@ -13,10 +13,10 @@ export type User = {
 export const users: Record<string, User> = usersData as Record<string, User>;
 
 export const prompts = {
-  blocked: readFileSync('./prompts/blocked.txt', 'utf-8'),
-  system: readFileSync('./prompts/system.txt', 'utf-8'),
-  guardrails: readFileSync('./prompts/guardrails.txt', 'utf-8'),
-}
+  blocked: readFileSync("./prompts/blocked.txt", "utf-8"),
+  system: readFileSync("./prompts/system.txt", "utf-8"),
+  guardrails: readFileSync("./prompts/guardrails.txt", "utf-8"),
+};
 
 // Model configuration type
 export type ModelConfig = {
@@ -35,25 +35,26 @@ export type ModelConfig = {
   temperature: number;
   maxTokens: number;
   guardrailsModel: string;
-
 };
 
 export const config: ModelConfig = {
   apiKey: process.env.OPENROUTER_API_KEY!,
-  httpReferer: '',
-  xTitle: 'IA Devs - Guardrails'!,
+  httpReferer: "",
+  xTitle: "IA Devs - Guardrails"!,
   models: [
     // 'qwen/qwen-2.5-7b-instruct',
     // 'qwen/qwen3-coder-next',
     // 'upstage/solar-pro-3:free',
-    'qwen/qwen-2.5-7b-instruct',// unsafe!
+    // 'qwen/qwen-2.5-7b-instruct',// unsafe!
+    "baidu/cobuddy:free", // unsafe!
   ],
 
-  guardrailsModel: 'openai/gpt-oss-safeguard-20b',
+  // guardrailsModel: "openai/gpt-oss-safeguard-20b",
+  guardrailsModel: "openai/gpt-oss-120b:free",
   provider: {
     sort: {
-      by: 'price',
-      partition: 'none',
+      by: "price",
+      partition: "none",
     },
   },
   temperature: 0.7,
